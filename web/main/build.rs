80315b4aae3810fafbf44f48815dac2f30ee0ce1
Jazzpirate <raupi@jazzpirate.com>
2024-07-29 09:43:03 +0200

    fix

--- web/main/build.rs
+++ web/main/build.rs
@@ -1,15 +1,28 @@
-use std::path::Path;
 fn main() {
     if let Some(s) = std::env::var("IMMT_BUILD_GRAPHS").ok() {
         if s.eq_ignore_ascii_case("true") {} else { return }
     } else { return }
+    let p = if let Ok(p) = std::env::current_dir() {
+        if let Some(s) = p.as_os_str().to_str() {
+            if s.ends_with("main") {
+                p
+            } else {
+                panic!("Wrong build directory")
+            }
+        } else {
+            panic!("Wrong build directory")
+        }
+    } else {
+        panic!("Wrong build directory")
+    };
+    let main = p.parent().expect("Wrong build directory").parent().expect("Wrong build directory");
     //println!("cargo::rerun-if-changed=../graphs");
     std::process::Command::new("trunk")
-        .env("CARGO_TARGET_DIR","../../target")
+        .env("CARGO_TARGET_DIR",main.join("target"))
         .env("RUSTFLAGS","--cfg=web_sys_unstable_apis")
-        .current_dir("../graphs")
-        .args(vec!["build","--features=client","--release"]).status().expect("trunk build failed!");
-    let target = Path::new("assets").join("graphs");
+        .current_dir(main.join("web").join("graphs"))
+        .args(vec!["build","--features=client","--release"]).stdout(std::io::stderr()).stderr(std::io::stderr()).status().expect("trunk build failed!");
+    let target = main.join("web").join("main").join("assets").join("graphs");
     let _ = std::fs::create_dir_all(&target);
     let mut replaces = vec![
         ("manifest.json".to_string(),"graph_viewer/manifest.json"),
@@ -18,7 +31,7 @@ fn main() {
     ];
     let mut js = String::new();
     let mut html = String::new();
-    for entry in std::fs::read_dir(Path::new("../graphs/dist")).unwrap() {
+    for entry in std::fs::read_dir(main.join("web/graphs/dist")).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
         let mut file_name = path.file_name().unwrap().to_str().unwrap();
