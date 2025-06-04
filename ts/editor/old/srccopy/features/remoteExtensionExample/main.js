93b9e29a4348f3fc085891ace490d95c086713b2
Jazzpirate <raupi@jazzpirate.com>
2025-06-04 17:56:53 +0200

    deleted nonsense

--- ts/editor/old/srccopy/features/remoteExtensionExample/main.js
+++
@@ -1,8 +0,0 @@
-const vscode = require('vscode')
-const os = require("os")
-
-void vscode.window.showInformationMessage('Hello', {
-  detail: `Hello from remote extension running from ${os.hostname()}`,
-  modal: true
-})
-
