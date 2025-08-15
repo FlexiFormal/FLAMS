35a682a59fc8229e9591d2c2343f4f9616de44b0
Jazzpirate <raupi@jazzpirate.com>
2025-08-15 07:16:18 +0200

    math-archives

---
+++ math-archives/src/lib.rs
@@ -0,0 +1,7 @@
+#![allow(unexpected_cfgs)]
+#![cfg_attr(all(doc, CHANNEL_NIGHTLY), feature(doc_auto_cfg))]
+#![doc = include_str!("../README.md")]
+/*!
+ * ## Feature flags
+ */
+#![cfg_attr(doc,doc = document_features::document_features!())]
