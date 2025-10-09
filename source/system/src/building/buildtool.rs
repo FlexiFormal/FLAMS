adf4333271d4443170ffca653f83327808566f18
praveen <praveenpp2929@gmail.com>
2025-10-09 17:23:57 +0200

    commit here

--- source/system/src/building/buildtool.rs
+++ source/system/src/building/buildtool.rs
@@ -4,9 +4,31 @@ use super::{
 };
 
 mod tests {
-    use crate::building::BuildTask;
+    use crate::building::{BuildTask, TaskState};
 
     use super::*;
 
-    pub fn test_buildtool() {}
+    pub fn test_buildtool(map: &TaskMap, state: &mut RunningQueue) {
+        let RunningQueue {
+            queue,
+            running,
+            blocked,
+            done,
+            failed,
+            ..
+        } = state;
+        let tasks = map.map.values().collect::<Vec<_>>();
+        while !tasks.is_empty() {
+            for i in &tasks {
+                if let Some(x) = i.steps().iter().any(|st| {
+                    let ans = st.0.state.read();
+                    *ans == TaskState::Failed
+                }) {
+                    failed.push(i.clone());
+                }
+            }
+        }
+        // now in each build task there is some kind of dependency with other build task
+        // there is no failure assumption
+    }
 }
